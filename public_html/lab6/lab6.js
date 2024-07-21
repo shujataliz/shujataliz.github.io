2c7bfa35ac49c884ee7cd3d9d0a7177ba6781337
Shujat Ali <Shujat.dev@gmail.com>
2024-07-21 01:15:15 -0500

    done lab6

--- public_html/lab6/lab6.js
+++ public_html/lab6/lab6.js
@@ -67,13 +67,12 @@
 
             const Person = function () {
             };
-            Person.prototype.initialize = function (name, age) {
+            Person.prototype.initialize = function (name) {
                 this.name = name;
-                this.age = age;
             }
 
             Person.prototype.describe = function () {
-                return this.name + ", " + this.age + " years old.";
+                return "Person name is "+ this.name;
             }
             
     
@@ -85,7 +84,7 @@
             }
     
             const prof = new Teacher();
-            prof.initialize("Taha", 40);
+            prof.initialize("Taha");
             console.log(prof.describe());
             console.log(prof.teach("Inheritance"));
 
@@ -95,14 +94,17 @@
             ‘name’ to be set in the prototype.
                 `);
 
-            const me2 = {
-                name: 'teacher',
-                age: 32,
-                describe: function() { return this.name + " is " + this.age + " years old"; }
-            }
+            const Professor = Object.create(Teacher);
+            //factory function
+            const  createProfessor = function(name) {
+                const prof = Object.create(Professor.prototype)
+                prof.initialize(name);
+                return prof
 
-            const person2 = Object.create(me2);
-            console.log(person2.describe());
+            }
+            const prof2 = createProfessor("Jackson")
+            console.log(prof2.describe());
+            console.log(prof2.teach("Database"));
 
 
             console.log(`
@@ -121,22 +123,100 @@
                 `);
 
                 
-                const Student = function () {
+                Person.prototype.initialize = function (name, age) {
+                    this.name = name;
+                    this.age = age;
+                }
+
+                Person.prototype.greet = function () {
+                    return `Greetings, my name is ${name} and I am ${age} years old`;
+                }
+
+                Person.prototype.salute = function () {
+                    function greetingMsg() {
+                        const now = new Date();
+                        const hour = now.getHours();
+                        
+                        if (hour < 12) {
+                            return "Good morning";
+                        } else if (hour < 18) {
+                            return "Good afternoon";
+                        } else {
+                            return "Good evening";
+                        }
+                    }
+                    return `${greetingMsg()}! and in case i don't see you`;
+                }
+
+                console.log(`
+                        Student objects inherit name, age, and salute from person. They also have a field
+                        ‘major’ and have their own greeting method. Their greeting is “Hey, my name is
+                        [name] and I am studying [major]. The greeting should be output to the console.
+                    `);
+
+                const Student = function() {
+                    Person.call(this);
+                }
+                
+                Student.prototype = Object.create(Person.prototype);
+                Student.prototype.constructor = Student;
+                Student.prototype.initialize = function(name, age, major) {
+                    Person.prototype.initialize.call(this, name, age);
+                    this.major = major;
                 };
-                Student.prototype = new Person();
-                Student.prototype.learn = function (subject) {
-                    return this.name + " just learned " + subject;
+
+                
+                Student.prototype.greet = function (subject) {
+                    return `Hey, my name is ${this.name} and i am studying ${this.major}`;
                 }
                 const me = new Student();
-                me.initialize("Ali", 32);
+                me.initialize("Ali", 32, "Computer Science");
                 console.log(me.describe());
-                console.log(me.learn("Inheritance"));
+                console.log(me.greet());
+                console.log(me.salute());
 
-            
+                
+                console.log(`
+                    Professor objects inherit name, age, and salute from person. They also have a field
+                    ‘department’ and have their own greeting method. Their salutation is “Good day,
+                    my name is [name] and I am in the [department] department.” Output it to the
+                    console.
+                    `);
+
+                    const CustomProfessor = function() {
+                        Person.call(this);
+                    }
+                    
+                    CustomProfessor.prototype = Object.create(Person.prototype);
+                    CustomProfessor.prototype.constructor = CustomProfessor;
+                    CustomProfessor.prototype.initialize = function(name, age, department) {
+                        Person.prototype.initialize.call(this, name, age);
+                        this.department = department;
+                    };
+                    CustomProfessor.prototype.salute = function() {
+                        return `Good day, my name is ${this.name} and I am in the ${this.department} department`;
+                    };
+
+                    const prof1 = new CustomProfessor();
+                    prof1.initialize("Michael", 35, "Enterprise Architecture");
+                    console.log(prof1.salute());
 
 
+                    console.log(`
+                    Create a professor object and a student object. Call both the greeting and salutation
+                    methods on each.
+                        `);
 
-
+                    const professor1 = new CustomProfessor();
+                    professor1.initialize("Jordan",40, "Sports");
+                    
+                    const student1 = new Student();
+                    student1.initialize("Jack", 20, "Football");
+                    
+                    console.log(professor1.salute());
+                    
+                    console.log(student1.greet());
+                    console.log(student1.salute());
 
 }
 )();
